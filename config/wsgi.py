4161589edc093ce58138bdcab497406350a69194
HamXa <hamza.wasym99@gmail.com>
2025-04-19 01:45:07 +0500

    postgresConfigured5

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,19 +1,8 @@
-"""
-WSGI config for django_boilerplate project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 from django.core.wsgi import get_wsgi_application
-
 from config.setup_environment import setup_environment
-import os
+
+# Set up the environment (this will set DJANGO_SETTINGS_MODULE)
 setup_environment()
 
+# Initialize the WSGI application
 application = get_wsgi_application()
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')  # Must match!
-
-app = application
